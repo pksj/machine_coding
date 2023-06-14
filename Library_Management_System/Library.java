3565f4574c0962cc2d80b0cdb1abd1f6e395da61
Pawankumar Jawale <pawankumar.jawale@gmail.com>
2023-06-14 17:20:20 +0530

    initial library

--- Library_Management_System/Library.java
+++ Library_Management_System/Library.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 public class Library {
   int id;
+  int numberOfRacks;
   List<Rack> racks;
   List<User> users;
 
-  public Library(int id) {
+  public Library(int id, int numberOfRacks) {
     this.id = id;
+    this.numberOfRacks = numberOfRacks;
   }
 
   public void addRack(Rack rack) {
